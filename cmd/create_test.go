9719749393eb2f412ca9f286f511a21c4f52e507
Chris Duncan <chris.duncan@plusworx.uk>
2020-06-28 14:47:18 +0100

    Add more test cases

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -6,22 +6,54 @@ import (
 
 func TestDoCreateUser(t *testing.T) {
 	cases := []struct {
-		args     []string
-		expected string
+		args          []string
+		attributes    string
+		expected      string
+		firstname     string
+		lastname      string
+		password      string
+		recoveryPhone string
 	}{
 		{
-
-			args:     []string{"mickey.mouse@dev.plusworx.uk", "-f", "Mickey"},
-			expected: "gmin: error - firstname, lastname and password must all be provided",
+			args:          []string{"mickey.mouse@disney.com"},
+			recoveryPhone: "988787686",
+			expected:      "gmin: error - recovery phone number 988787686 must start with '+'",
+		},
+		{
+			args:      []string{"mickey.mouse@disney.com"},
+			firstname: "Mickey",
+			lastname:  "Mouse",
+			expected:  "gmin: error - firstname, lastname and password must all be provided",
+		},
+		{
+			args:       []string{"mickey.mouse@disney.com"},
+			attributes: "email",
+			firstname:  "Mickey",
+			lastname:   "Mouse",
+			password:   "SuperStrongPassword",
+			expected:   "gmin: error - malformed attribute string",
+		},
+		{
+			args:       []string{"mickey.mouse@disney.com"},
+			attributes: "jklkjf:kljkjf",
+			firstname:  "Mickey",
+			lastname:   "Mouse",
+			password:   "SuperStrongPassword",
+			expected:   "gmin: error - attribute jklkjf not recognised",
 		},
 	}
 
 	for _, c := range cases {
+		attrs = c.attributes
+		firstName = c.firstname
+		lastName = c.lastname
+		password = c.password
+		recoveryPhone = c.recoveryPhone
+
 		got := doCreateUser(createUserCmd, c.args)
 
 		if got.Error() != c.expected {
 			t.Errorf("Expected error %v, got %v", c.expected, got.Error())
 		}
 	}
-
 }
