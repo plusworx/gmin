5446fd41da6d63bf9c4cd79859493bb28b39280c
Chris Duncan <chris.duncan@plusworx.uk>
2020-07-16 17:17:42 +0100

    Update: Bump version number to v0.5.0

--- cmd/root.go
+++ cmd/root.go
@@ -81,7 +81,7 @@ var rootCmd = &cobra.Command{
 	Long: `gmin is a commandline interface (CLI) that enables the 
 	administration of G Suite domains. It aims to provide tools that
 	make G Suite administration from the commandline more manageable.`,
-	Version: "v0.4.0",
+	Version: "v0.5.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
